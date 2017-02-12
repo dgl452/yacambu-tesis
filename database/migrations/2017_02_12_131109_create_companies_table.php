8dc014812225362fd470017fc945bc967035bad4
Ricardo Martos <ricardomartoslecea@gmail.com>
2017-02-12 09:14:56 -0400

    creacion de la tabla companies

---
+++ database/migrations/2017_02_12_131109_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->char('name');
+            $table->integer('gold');
+            $table->integer('silver');
+            $table->integer('bronce');
+            $table->integer('bonus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
